9ba8ea9e31e9f9a9acb8f0acd20b215e7e1c3f8e
developer980 <tudordin2002@gmail.com>
2023-04-22 21:49:00 +0300

    minor fixes

--- backend/api/app.py
+++ backend/api/app.py
@@ -15,11 +15,10 @@ from similarities.get_percentage import get_percentage
 
 import requests
 from bs4 import BeautifulSoup
-# "C:\Users\tudor\AppData\Roaming\nltk_data\corpora"
 
 
 nltk.download('punkt')
-nltk.download('stopwords')
+nltk.download('stopwords', download_dir="/usr/local/share/nltk_data")
 nltk.download('brown')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
